854b795c7369bd28b880dc61016617ef86983c3e
parkderick <derickmpark@gmail.com>
2019-10-21 12:02:24 -0700

    Finish Recursion Review

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+
+
+  var elementClass = function(data) {
+    if (data.classList && data.classList.contains(className)) {
+      result.push(data);
+    }
+    for (var i = 0; i < data.childNodes.length; i++) {
+      elementClass(data.childNodes[i]);
+    }
+  }
+
+  elementClass(document.body);
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,41 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  var answer = "";
+
+  if (typeof obj === "number" || typeof obj === "boolean") {
+    answer += obj;
+  }
+
+  if (obj === null) {
+    return "null";
+  }
+
+  if (typeof obj === "string") {
+    answer += '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    var recursionArray = [];
+    for (var i = 0; i < obj.length; i++) {
+      recursionArray.push(stringifyJSON(obj[i]));
+    }
+    answer += "[" + recursionArray + "]";
+
+  } else if (typeof obj === "object") {
+    answer += "{"
+    for (var key in obj) {
+      if (typeof obj[key] !== "function" && typeof obj[key] !== "undefined") {
+        answer += stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",";
+      }
+    }
+    if (answer !== "{") {
+      answer = answer.substring(0, answer.length - 1);
+    }
+    answer += "}";
+  }
+
+  return answer;
+
   // your code goes here
 };
